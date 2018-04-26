d2e122015577a08ddf993f6feb1529c9f469b043
mxdws <26325269+mxdws@users.noreply.github.com>
2018-04-26 23:05:37 +0100

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var request     = require('request');
+var fs          = require('fs');
+var http        = require('http');
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+/**
+ * Try and load data from api using axios library
+ */
+
+gulp.task('getData', function() {
+
+  console.log("Getting football data from api");
+
+  var token = '591024a7b9c74f66b6a079e9d34a0f04';
+
+  var options = {
+    'method': 'GET',
+    'hostname': 'api.football-data.org',
+    'path': '/v1/soccerseasons/446/fixtures',
+    'headers': {
+      'X-Auth-Token': token
+    }
+  };
+
+  http.get(options, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+
+      // Try and write the result to a data file
+
+      fs.writeFile('./_data/footballdata.json', body, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Comments data saved.");
+        }
+      });
+
+    });
+  //})
+  //.on('error', function(e) {
+  //  console.log("Got error: ", e);
+  });
+
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('_scss/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
